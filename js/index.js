aa637326b7f8bc2a9cb9932b4fe2a54071aef1e5
dechesan <deche93@outlook.com>
2024-04-05 22:46:18 -0600

    Made the scroll indicator turn transparent as you scroll down, and return to normal as you scroll up. However, it is currently being overridden by the fadeIn CSS animation.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
-window.onload = function() {
-    var scrollIndicator = document.getElementById("scroll-indicator");
+const scrollIndicator = document.getElementById("scroll-indicator");
+const indicatorTop = scrollIndicator.offsetTop;
 
-    window.addEventListener("scroll", function() {
-        var currentScroll = window.scrollY || document.documentElement.scrollTop;
-        if (currentScroll > lastScrollTop) {
-        // Scroll down
+window.addEventListener("scroll", function() {
+    scrollIndicator.style.animationPlayState
+
+    let indicatorOpacity = 1 - (window.scrollY / indicatorTop) * 2;
+
+    console.log(`Number: ${indicatorOpacity}\nString: ${indicatorOpacity.toString()}`);
+
+    if (indicatorOpacity < 0) {
         scrollIndicator.style.opacity = "0";
-        } else {
-        // Scroll up
-        scrollIndicator.style.opacity = "1";
-        }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-    });
-};
+    }
+    else {
+        scrollIndicator.style.opacity = indicatorOpacity.toString();
+    }
+    console.log(`Opacity: ${scrollIndicator.style.opacity}`);
+});
